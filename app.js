c3c020845512fd25ee6c33830657f485f691355a
spirit-B <janghoon333@naver.com>
2022-04-21 23:33:06 +0900

    section3-4 complete

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-// 문제 풀이용 js 파일
-function solution(s) {
-    let answer = Number(s.replace(/[\D]/g, ""));
+function solution(s, t) {
+    let answer = [];
+
+    for (let i = 0; i < s.length; i++) { // 각 문자들을 하나씩 탐색
+        let arr = []; // 여러개의 t와의 거리를 담을 배열
+        for (let j = s.length - 1; j >= 0; j--) { // 문자열의 맨 끝부터 탐색
+            if (s[j] === t) arr.push(Math.abs(j - i)); // 여러 t와의 거리값을 arr에 넣어줌
+        }
+        answer.push(Math.min(...arr)); // arr에서 최솟값을 answer에 넣어줌
+    }
+
     return answer;
 }
 
-let str = "g0en2T0s8eSoft";
-console.log(solution(str));
+let str = "teachermode";
+let letter = "e";
+console.log(solution(str, letter));
