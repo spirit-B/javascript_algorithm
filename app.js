e7a5b2210cf4f55ddad84fc73c9e810321fa7078
spirit-B <janghoon333@naver.com>
2022-04-22 22:13:10 +0900

    Section 3-5 complete

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-function solution(s, t) {
-    let answer = [];
+function solution(s) {
+    let answer = "";
+    let count = 1;
+    let temp_str = s[0];
 
-    for (let i = 0; i < s.length; i++) { // 각 문자들을 하나씩 탐색
-        let arr = []; // 여러개의 t와의 거리를 담을 배열
-        for (let j = s.length - 1; j >= 0; j--) { // 문자열의 맨 끝부터 탐색
-            if (s[j] === t) arr.push(Math.abs(j - i)); // 여러 t와의 거리값을 arr에 넣어줌
+    for (let i = 1; i <= s.length; i++) {
+        if (s[i] === temp_str) count++
+        else {
+            answer += temp_str;
+            if (count !== 1) {
+                answer += count;
+            }
+            temp_str = s[i];
+            count = 1;
         }
-        answer.push(Math.min(...arr)); // arr에서 최솟값을 answer에 넣어줌
     }
 
     return answer;
 }
 
-let str = "teachermode";
-let letter = "e";
-console.log(solution(str, letter));
+let str = "KKHSSSSSSSE";
+console.log(solution(str));
